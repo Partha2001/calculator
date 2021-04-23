a973b4db535c46afcf1ab38c2fd1c40a6a42a0eb
Partha Protim Sarkar <79684762+Partha2001@users.noreply.github.com>
2021-04-24 00:07:47 +0530

    done

---
+++ script.js
@@ -0,0 +1,16 @@
+function dis(val){
+    document.getElementById("result").value+=val;
+    console.log(val);
+}
+function solve(){
+    var x=document.getElementById("result").value;
+    var y=eval(x);
+    document.getElementById("result").value=y;
+}
+function clr(){
+    document.getElementById("result").value="";
+}
+function back(){
+    var value = document.getElementById("result").value;
+    document.getElementById("result").value = value.substr(0, value.length - 1);
+}
